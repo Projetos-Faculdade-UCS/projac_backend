7984e6658268a43a39bf400e7d5ce901bcdae76c
Samuel Frederico <sfrederico@ucs.br>
2024-05-28 10:30:39 -0300

    refactor: :fire: Remove TesteAuthApiView from urls.py and views.py

--- app/projac/urls.py
+++ app/projac/urls.py
@@ -1,12 +1,10 @@
 from rest_framework import routers
 from django.urls import path, include
-from projac.views import TesteAuthApiView
+
 
 router = routers.DefaultRouter()
 
 
-
 urlpatterns = [
     path('', include(router.urls)),
-    path('teste-auth/', TesteAuthApiView.as_view(), name='teste-auth'),
 ]
--- app/projac/views.py
+++ app/projac/views.py
@@ -1,12 +1,1 @@
-from rest_framework.views import APIView 
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_api_key.permissions import HasAPIKey
-
 # Create your views here.
-
-class TesteAuthApiView(APIView):
-    permission_classes = [HasAPIKey | IsAuthenticated]
-
-    def get(self, request):
-        return Response({'message': 'hello world!'})

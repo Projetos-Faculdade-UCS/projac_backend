783869fa25abd213abffbf8d0b4848d1e5d0a98c
Samuel Frederico <sfrederico@ucs.br>
2024-06-21 14:48:16 -0300

    chore: :bricks: Add Dockerfile and build scripts

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t projac:0.1 .
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 app/manage.py runserver 0.0.0.0:8000
+# python3 app/manage.py runserver 0.0.0.0:8000
+docker run -it -p 8000:8000 projac:0.1
